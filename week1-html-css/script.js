4797aab7730b45acdb5885dec05239f0a5c3bd4a
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-06-28 14:36:46 +0600

    Fix string interpolation syntax in console logs

--- week1-html-css/script.js
+++ week1-html-css/script.js
@@ -8,14 +8,14 @@ let currentSkills = "HTML and CSS";
 // Log them to console
 console.log("Name: " + myName);
 console.log("Current Job: " + myJob)
-console.log('My goal is to become a ${myGoal} by ${learningYear}');
+console.log(`My goal is to become a ${myGoal} by ${learningYear}`);
 console.log("Skills so far: " + currentSkills);
 
 // Simple math
 let hoursPerDay = 4;
 let daysPerWeek = 5;
 let weeklyHours = hoursPerDay * daysPerWeek;
-console.log(' I study ${weeklyHours} hours per week');
+console.log(`I study ${weeklyHours} hours per week`);
 
 // Update a let variable
 currentSkills = "HTML, CSS and Javascript";
