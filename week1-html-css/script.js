1276a9708748448f012a3078960c146b26ffe306
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-06-30 17:03:51 +0600

    Update learning days logic and console output
    Fix variable name and improve console messages.

--- week1-html-css/script.js
+++ week1-html-css/script.js
@@ -3,3 +3,13 @@ let age = 35;
 let daysLearning = 8;
 
 console.log(`My name is ${name}, I am ${age} years old, and I've been learning for ${daysLearning} days.`);
+
+let dayslearning = 9;
+
+if (dayslearning < 7) {
+  console.log(`Day ${dayslearning}: Just getting started`);
+} else if (dayslearning >= 7 && dayslearning <=30) {
+  console.log(`Day $(dayslearning}: Building momentum`);
+} else {
+  console.log(`Day ${dayslearning}:Solid Progress`);
+}  
