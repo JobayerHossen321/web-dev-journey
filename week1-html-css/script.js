8abeb933b0a9daca4416e91f5f8acc84d2ea97c5
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-06-30 16:06:43 +0600

    Fix daysLearning variable to use correct format

--- week1-html-css/script.js
+++ week1-html-css/script.js
@@ -1,5 +1,5 @@
 const name = "Jobayer";
 let age = 35;
-let daysLearning = 08;
+let daysLearning = 8;
 
 console.log(`My name is ${name}, I am ${age} years old, and I've been learning for ${daysLearning} days.`);
