9741462d41a3712c88ecbebbd3757d8ab8ed0aac
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-07-07 14:54:45 +0600

    Add event listener to reveal secret text

--- week3-dom/script.js
+++ week3-dom/script.js
@@ -3,3 +3,7 @@ const revealBtn = document.getElementById('revealBtn');
 const secretText = document.getElementById('secretText');
 
 secretText.style.display = 'block';
+
+revealBtn.addEventListener(`click`, (`The password in pineapple`) => {
+  secretText.classList.remove(`hidden`);
+});
