9759e0e3b0e9277e9cf43dcb06488fd73f4048a6
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-07-07 16:08:42 +0600

    Add toggle functionality for secret text display
    Toggle display of secret text on button click.

--- week3-dom/script.js
+++ week3-dom/script.js
@@ -4,3 +4,9 @@ const secretText = document.getElementById('secretText');
 revealBtn.addEventListener('click', () => {
   secretText.style.display = 'block';
 });
+
+if (secretText.style.display === 'block') {
+  secretText.style.display = 'none';
+} else {
+  secretText.style.display = 'block';
+}
