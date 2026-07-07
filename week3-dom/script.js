5f1e52202490d1c2b478a34010fb8754a617f866
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-07-07 16:55:13 +0600

    Update script.js

--- week3-dom/script.js
+++ week3-dom/script.js
@@ -2,13 +2,10 @@ const revealBtn = document.getElementById('revealBtn');
 const secretText = document.getElementById('secretText');
 
 revealBtn.addEventListener('click', () => {
-  secretText.style.display = 'block';
-
-  if (secretText.style.display === 'block') {
-  secretText.style.display = 'none';
-} else {
-  secretText.style.display = 'block';
-}  
+   if (secretText.style.display === 'none' || secretText.style.display === '') {
+    secretText.style.display = 'block';
+  } else {
+    secretText.style.display = 'none';
+  }  
 });
-
 
