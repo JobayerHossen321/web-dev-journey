3dd72ca6e396b43ba83ba6e204610ebef31266c4
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-07-08 17:15:09 +0600

    Refactor revealAllBtn click event handler

--- week3-dom/script.js
+++ week3-dom/script.js
@@ -12,16 +12,11 @@ revealBtn.addEventListener('click', () => {
 const revealAllBtn = document.getElementById('revealAllBtn');
 const hiddenItems = document.querySelectorAll('.hidden-item');
 
-console.log(hiddenItems);
-
 revealAllBtn.addEventListener('click', function () {
   hiddenItems.forEach(function (item) {
     item.style.display = 'block';
   });
-});
-
-hiddenItems.forEach(item => {
-  if (item.style.display === 'none' || item.style.display === '') {
+    if (item.style.display === 'none' || item.style.display === '') {
     item.style.display = 'block';
   } else {
     item.style.display = 'none';
