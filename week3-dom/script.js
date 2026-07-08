612a83766f3351c62af40daca9e2e207ef326f40
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-07-08 17:31:14 +0600

    Refactor revealAllBtn click handler to toggle visibility

--- week3-dom/script.js
+++ week3-dom/script.js
@@ -13,9 +13,10 @@ const revealAllBtn = document.getElementById('revealAllBtn');
 const hiddenItems = document.querySelectorAll('.hidden-item');
 
 revealAllBtn.addEventListener('click', function () {
-  if (item.style.display === 'none' || item.style.display === '') {
+  hiddenItems.forEach(item => {
+   if (item.style.display === 'none' || item.style.display === '') {
     item.style.display = 'block';
   } else {
     item.style.display = 'none'; 
-  });
+  }});   
   });
