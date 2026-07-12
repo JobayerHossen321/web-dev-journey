10fcef920feefc738e7cc428033a656d9a7ddc4f
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-07-12 15:17:09 +0600

    Implement dynamic greeting in script.js
    Add dynamic greeting based on current hour.

--- week2-project-interactive-profile-page/script.js
+++ week2-project-interactive-profile-page/script.js
@@ -1,1 +1,18 @@
+// 1. Grab the heading element
+const heading = document.getElementById('welcome-heading');
 
+// 2. Get the current hour (0 to 23) using the Date object
+const currentHour = new Date().getHours();
+let greeting = "";
+
+// 3. Determine the greeting based on the hour
+if (currentHour < 12) {
+  greeting = "Good morning";
+} else if (currentHour < 18) {
+  greeting = "Good afternoon";
+} else {
+  greeting = "Good evening";
+}
+
+// 4. Update the text dynamically!
+heading.textContent = `${greeting}, I am Jobayer Hossen`;
