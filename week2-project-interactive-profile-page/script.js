1c44a3fdf05f1fab304b4a3f2ccd0719b25592b0
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-07-12 16:02:39 +0600

    Add interactive profile page functionality

--- week2-project-interactive-profile-page/script.js
+++ week2-project-interactive-profile-page/script.js
@@ -1,3 +1,65 @@
+const name = "Jobayer";
+let age = 35;
+let daysLearning = 8;
+
+console.log(`My name is ${name}, I am ${age} years old, and I've been learning for ${daysLearning} days.`);
+
+daysLearning = 9;
+
+if (daysLearning < 7) {
+  console.log(`Day ${daysLearning}: Just getting started`);
+} else if (daysLearning >= 7 && daysLearning <=30) {
+  console.log(`Day ${daysLearning}: Building momentum`);
+} else {
+  console.log(`Day ${daysLearning}: Solid Progress`);
+}  
+
+let skills = ["HTML", "CSS", "JavaScript", "Python", "Git"];   //Creating the array
+
+for (let i = 0; i < skills.length; i++) {    //Loop to print each skill with its position
+  console.log(`${i+1}: ${skills[i]}`);
+}
+
+skills.push("React");  //Add "React" to the array
+
+// ৩. বড় শব্দ সরাসরি প্রিন্ট করার ফাংশন
+function printLongWords(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > 4) {
+      console.log(arr[i]);
+    }
+  } 
+}     
+printLongWords(skills);
+
+// ৪. বড় শব্দ নতুন ঝুড়িতে জমা করার ফাংশন (এখানে ভুল ঠিক করা হয়েছে)
+function getLongWords (words) {
+  let longWords = []; // ল্যাপটপকে বললাম: এই নাও একটা নতুন খালি ঝুড়ি
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      longWords.push(words[i]); // এখন খালি ঝুড়িতে স্কিল ঢুকবে
+    }  
+  }
+  return longWords; // ঝুড়িটা ফেরত দিলাম
+}
+let result = getLongWords(skills);
+console.log(result); // আউটপুট দেখাবে: ['JavaScript', 'React']
+
+// ৫. সব সংখ্যা যোগ করার ফাংশন
+function sumArray(numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
+}
+
+let myNumbers = [10, 25, 3, 7, 15];
+let totalSum = sumArray(myNumbers);
+console.log(totalSum); // আউটপুট দেখাবে: 60
+
+// Interactive Design
+
 // 1. Grab the heading element
 const heading = document.getElementById('welcome-heading');
 
