813a0b9c9e07fe43b48f713ab2a5be551e3ea2e9
JobayerHossen321 <hossenjobayer41@gmail.com>
2026-07-20 12:07:04 +0600

    Initialize quiz questions data structure
    Added an array of quiz questions with options and correct answers.

--- project-01-quiz-app/script.js
+++ project-01-quiz-app/script.js
@@ -1,1 +1,23 @@
-
+// Our Quiz Questions Array
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options: ["Java", "C", "Python", "JavaScript"],
+        correct: "JavaScript"
+    },
+    {
+        question: "What does HTML stand for?",
+        options: [
+            "Hypertext Markup Language",
+            "High Text Machine Language",
+            "Hyper Transfer Mark Language",
+            "Home Tool Markup Language"
+        ],
+        correct: "Hypertext Markup Language"
+    },
+    {
+        question: "Which symbol is used for strict equality in JS?",
+        options: ["=", "==", "===", "=>"],
+        correct: "==="
+    }
+];
